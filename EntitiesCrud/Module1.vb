511f2239037821badefe5106f22d9f938d0143a4
evanboissonnot <evan@boissonnot.fr>
2018-03-02 18:03:15 +0100

    Entities en cours RAF : add et update RAF : un peu de linq

---
+++ EntitiesCrud/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        Using context As New starwars_dbEntities1()
+            Dim query = From robot In context.Robot
+                        Select robot
+
+            For Each rob As Robot In query
+                Console.WriteLine(rob)
+            Next
+
+            For Each jedi As Jedi In context.Jedi
+                Console.WriteLine(jedi.Name)
+            Next
+
+            '
+
+
+            context.SaveChanges()
+        End Using
+
+        Console.ReadLine()
+    End Sub
+
+End Module
